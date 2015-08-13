d44a8eafe70a0c1cea3e7b25f15e455cda645688
Bryan Hockey <bhock@umich.edu>
2015-08-13 16:12:29 -0400

    configuration.rb

---
+++ lib/dpn/client/configuration.rb
@@ -0,0 +1,63 @@
+# Copyright (c) 2015 The Regents of the University of Michigan.
+# All Rights Reserved.
+# Licensed according to the terms of the Revised BSD License
+# See LICENSE.md for details.
+
+module DPN
+  module Client
+    module Configuration
+      @@keys = [
+          :api_root, :auth_token, :auto_paginate,
+          :per_page, :user_agent
+      ]
+
+      @@defaults = {
+          api_root: nil.freeze,
+          auth_token: nil.freeze,
+          auto_paginate: true.freeze,
+          per_page: 25.freeze,
+          user_agent: "dpn-client".freeze
+      }.freeze
+
+      attr_accessor :api_root, :auth_token, :auto_paginate,
+                    :per_page, :user_agent
+
+
+      # Apply the options hash to the configuration
+      def configure(options_hash)
+        Configuration.keys.each do |key|
+          instance_variable_set(:"@#{key}", options_hash[key])
+        end
+        if block_given?
+          yield self
+        end
+      end
+
+
+      # Reset to default values
+      def reset
+        Configuration.keys.each do |key|
+          instance_variable_set(:"@#{key}", Configuration.defaults[key])
+          self
+        end
+      end
+      alias setup reset
+
+      def base_url
+        File.join(@api_root, "api-v#{DPN::Client.api_version}")
+      end
+
+      protected
+
+      def defaults
+        @@defaults
+      end
+
+      def keys
+        @@keys
+      end
+
+
+    end
+  end
+end
