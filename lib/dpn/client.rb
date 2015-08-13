ba0523424d37a0e224b435ab955d87174175b2e4
Bryan Hockey <bhock@umich.edu>
2015-08-13 16:12:19 -0400

    module

---
+++ lib/dpn/client.rb
@@ -0,0 +1,23 @@
+# Copyright (c) 2015 The Regents of the University of Michigan.
+# All Rights Reserved.
+# Licensed according to the terms of the Revised BSD License
+# See LICENSE.md for details.
+
+require "dpn/client/client"
+
+module DPN
+  module Client
+
+    # Get the api version, based on the major version of this library.
+    def self.api_version
+      DPN::Client::VERSION.split(".")[0]
+    end
+
+
+    def client(*options)
+      return ::DPN::Client::Client.new options
+    end
+
+
+  end
+end
