a1d6195bca4a0def273657bbdee976fed5278075
Bryan Hockey <bhock@umich.edu>
2015-08-24 18:08:11 -0400

    basic tests for all 4 verbs

--- spec/dpn/client/agent/connection_spec.rb
+++ spec/dpn/client/agent/connection_spec.rb
@@ -6,46 +6,37 @@
 require "spec_helper"
 
 API_ROOT = "https://api.test.dpn-client.org/"
+url = File.join API_ROOT, "/"
 
-shared_examples "basic http" do |operation|
-  url = File.join API_ROOT, "/"
-  let(:body) { {tag: "content" } }
+shared_examples "basic http" do |operation, *args|
+  let(:body) {  {tag: "content" } }
   before(:each) do
-    @request = stub_request(:get, url)
+    @request = stub_request(operation, url)
                    .to_return(body: body.to_json, status: 200, headers: {content_type: "application/json"})
   end
 
-  it "queries #{url}" do
-    connection.public_send(operation, url)
+  it "sends the request to #{url}" do
+    connection.public_send(operation, url, *args)
     expect(@request).to have_been_requested
   end
 
   it "returns the correct response object" do
-    response = connection.public_send(operation, url)
+    response = connection.public_send(operation, url, *args)
     expect(response).to be_a DPN::Client::Response
     expect(response.body).to eql(body)
   end
 
   it "executes the passed block on the response" do
     expect{ |probe|
-      connection.public_send(operation, url, &probe)
+      connection.public_send(operation, url, *args, &probe)
     }.to yield_with_args(DPN::Client::Response)
-  end
-
-  it "handles query parameters" do
-    real_query = "a=1,2,3&b=foo"
-    @request.with(query: real_query)
-
-    connection.public_send(operation, url, { a: [1,2,3], b: "foo" } )
-
-    expect(@request).to have_been_requested
   end
 
   it "returns errors" do
     error_url = File.join url, "error", "/"
-    stub_request(:get, error_url).to_return(body: body.to_json, status: 400)
+    stub_request(operation, error_url).to_return(body: body.to_json, status: 400)
 
-    response = connection.public_send(operation, error_url)
+    response = connection.public_send(operation, error_url, *args)
 
     expect(response).to be_a DPN::Client::Response
     expect(response.status).to eql(400)
@@ -58,11 +49,29 @@ describe DPN::Client::Agent::Connection do
   let(:connection) { DPN::Client::Agent.new(api_root: API_ROOT, auth_token: "some_auth_token") }
 
   describe "#get" do
-    it_behaves_like "basic http", :get
+    it_behaves_like "basic http", :get, nil
+
+    it "handles query parameters" do
+      real_query = "a=1,2,3&b=foo"
+      stub = stub_request(:get, url).with(query: real_query)
+        .to_return(body: "{}", status: 200, headers: {content_type: "application/json"})
+
+      connection.get(url, { a: [1,2,3], b: "foo" } )
+
+      expect(stub).to have_been_requested
+    end
   end
 
   describe "#post" do
-    it_behaves_like "basic http", :get
+    it_behaves_like "basic http", :post, { foo: "bar" }.to_json
+  end
+
+  describe "#put" do
+    it_behaves_like "basic http", :put, { foo: "bar" }.to_json
+  end
+
+  describe "#delete" do
+    it_behaves_like "basic http", :delete
   end
 
 
