6300d865b2c149cc88a82bd1a4a55ba60d3916ff
Bryan Hockey <bhock@umich.edu>
2015-08-24 17:00:27 -0400

    shared examples

--- spec/dpn/client/agent/connection_spec.rb
+++ spec/dpn/client/agent/connection_spec.rb
@@ -7,48 +7,62 @@ require "spec_helper"
 
 API_ROOT = "https://api.test.dpn-client.org/"
 
+shared_examples "basic http" do |operation|
+  url = File.join API_ROOT, "/"
+  let(:body) { {tag: "content" } }
+  before(:each) do
+    @request = stub_request(:get, url)
+                   .to_return(body: body.to_json, status: 200, headers: {content_type: "application/json"})
+  end
+
+  it "queries #{url}" do
+    connection.public_send(operation, url)
+    expect(@request).to have_been_requested
+  end
+
+  it "returns the correct response object" do
+    response = connection.public_send(operation, url)
+    expect(response).to be_a DPN::Client::Response
+    expect(response.body).to eql(body)
+  end
+
+  it "executes the passed block on the response" do
+    expect{ |probe|
+      connection.public_send(operation, url, &probe)
+    }.to yield_with_args(DPN::Client::Response)
+  end
+
+  it "handles query parameters" do
+    real_query = "a=1,2,3&b=foo"
+    @request.with(query: real_query)
+
+    connection.public_send(operation, url, { a: [1,2,3], b: "foo" } )
+
+    expect(@request).to have_been_requested
+  end
+
+  it "returns errors" do
+    error_url = File.join url, "error", "/"
+    stub_request(:get, error_url).to_return(body: body.to_json, status: 400)
+
+    response = connection.public_send(operation, error_url)
+
+    expect(response).to be_a DPN::Client::Response
+    expect(response.status).to eql(400)
+  end
+end
+
+
 describe DPN::Client::Agent::Connection do
   before(:all) { WebMock.enable! }
   let(:connection) { DPN::Client::Agent.new(api_root: API_ROOT, auth_token: "some_auth_token") }
 
   describe "#get" do
-    url = File.join API_ROOT, "/"
-    body = {a: "b"}
-    before(:each) do
-      @request = stub_request(:get, url)
-                  .to_return(body: body.to_json, status: 200, headers: {content_type: "application/json"})
-    end
-
-    it "queries #{url}" do
-      connection.get(url)
-      expect(@request).to have_been_requested
-    end
-
-    it "returns the correct response object" do
-      response = connection.get(url)
-      expect(response).to be_a DPN::Client::Response
-      expect(response.body).to eql(body)
-    end
-
-    it "executes the passed block on the response" do
-      expect{ |probe|
-        connection.get(url, &probe)
-      }.to yield_with_args(DPN::Client::Response)
-    end
-
-    it "handles query parameters" do
-      real_query = "a=1,2,3&b=foo"
-      @request.with(query: real_query)
-
-      connection.get(url, { a: [1,2,3], b: "foo" } )
-
-      expect(@request).to have_been_requested
-    end
+    it_behaves_like "basic http", :get
   end
 
-
   describe "#post" do
-
+    it_behaves_like "basic http", :get
   end
 
 
