0d08cb6d30525670da38172baf03360d827ad46f
Bryan Hockey <bhock@umich.edu>
2015-08-25 17:17:25 -0400

    shared examples for standard operations of dpn-client methods now in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,5 +27,32 @@ def dpn_url(url)
   else
     File.join test_api_root, api_string, url, "/"
   end
+end
 
+shared_examples "a dpn-client method" do |method, *args|
+  # expect stub to be defined with let!
+
+  it "sends the right request" do
+    agent.public_send(method, *args)
+    expect(stub).to have_been_requested
+  end
+
+  it "returns a response" do
+    response = agent.public_send(method, *args)
+    expect(response).to be_a DPN::Client::Response
+    expect(response.to_json).to eql(stub.response.body)
+  end
+
+  it "calls the block" do
+    expect { |probe|
+      agent.public_send(method, *args, &probe)
+    }.to yield_with_args(be_a(DPN::Client::Response))
+  end
+
+  it "yields the response" do
+    agent.public_send(method, *args) do |response|
+      expect(response.status).to eql(stub.response.status[0])
+      expect(response.to_json).to eql(stub.response.body)
+    end
+  end
 end
